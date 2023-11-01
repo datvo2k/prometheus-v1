149ffbc2079e4c3daee20bede84115b4603214d2
Brian Vo <datvo2k@gmail.com>
2023-11-02 00:23:56 +0700

    add prometheus mainfests

---
+++ grafana-cluster/installCoresPrometheusOperator.sh
@@ -0,0 +1,5 @@
+#!/bin/bash  
+
+# Install the operator
+LATEST=$(curl -s https://api.github.com/repos/prometheus-operator/prometheus-operator/releases/latest | jq -cr .tag_name)
+curl -sL https://github.com/prometheus-operator/prometheus-operator/releases/download/${LATEST}/bundle.yaml | kubectl create -f - -n monitoring
